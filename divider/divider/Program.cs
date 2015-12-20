df09e3c33ea4637950e3409a5e2b4ae4eed9de86
regedik <regedik22@gmail.com>
2015-12-21 01:34:31 +0200

    final
    deviders

---
+++ divider/divider/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace divider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Input number: ");
+
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                num = 0;
+            }
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            
+            Console.ReadKey(true);
+        }
+    }
+}
